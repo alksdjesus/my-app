c12c295f49ff60979233f7e2b56c077ca49a8e9b
alksdjesus <alksdjesus@gmail.com>
2022-10-19 18:29:42 -0400

    clock functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import logo from './logo.svg';
+import './index.css';
 import './App.css';
 
 function App() {
+
+  function getGreeting(user) {
+    if (user) {
+      return <h1>Hello, {formatName(user)}!</h1>;
+    }
+    return <h1>Hello, Stranger.</h1>;
+  }  
+
+  function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+  }
+  
+  const user = {
+    firstName: 'Aleksandre',
+    lastName: 'De Jesus'
+  };
+  
+  const element1 = (
+    <h1>
+      Good afternoon, {formatName(user)}
+    </h1>
+  );
+
+  const name = 'Aleksandre';
+  const element = <h1>Good evening, {name}</h1>;
+
+  
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          {element}
           Edit <code>src/App.js</code> and save to reload.
+          Hello! This is my first edit on my React App.
         </p>
         <a
           className="App-link"
